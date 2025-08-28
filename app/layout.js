eb24f33112c367ee4d55d66c0e126f6e3dd89984
Legion204 <ri46801@gmail.com>
2025-08-28 22:29:14 +0600

    responsive bug fix

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Head from 'next/head';
 
 
 
@@ -18,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body
         className={inter.className}
       >
